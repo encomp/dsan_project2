35baf267647be04d4868a8c4e1c7a51d3e9f3044
encomp <edgar.martinez.rico@gmail.com>
2019-08-17 16:38:01 -0700

    first commit

--- project2/problem_2/FileRecursionTest.py
+++ project2/problem_2/FileRecursionTest.py
@@ -1,11 +1,26 @@
 import unittest
+
 from project2.problem_2.FileRecursion import find_files
 
 
-class FileRecursionTestCase(unittest.TestCase):
+class FileRecursionTest(unittest.TestCase):
 
-    def test_something(self):
-        print(find_files(".c", "/Users/edgarrico/Downloads/testdir"))
+    def test_suffix_c(self):
+        expected = ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c',
+                    './testdir/subdir1/a.c']
+        self.assertTrue(find_files(".c", "."), expected)
+
+    def test_suffix_h(self):
+        expected = ['./testdir/subdir3/subsubdir1/b.h', './testdir/subdir5/a.h', './testdir/t1.h',
+                    './testdir/subdir1/a.h']
+        self.assertTrue(find_files(".h", "."), expected)
+
+    def test_none_path(self):
+        self.assertIsNone(find_files(".g", None))
+        self.assertIsNone(find_files(".g", ""))
+
+    def test_empty_result(self):
+        self.assertEqual(find_files(".java", "."), [])
 
 
 if __name__ == '__main__':
